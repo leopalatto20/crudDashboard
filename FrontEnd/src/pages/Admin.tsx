a6806de7c14fa496db3d8a020fa0e84df2f3002e
BarbieMunoz <a01659348@tec.mx>
2025-04-09 11:03:30 -0600

    si

--- FrontEnd/src/pages/Admin.tsx
+++ FrontEnd/src/pages/Admin.tsx
@@ -1,7 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import Student from '../components/Student';
+
+interface Alumno {
+  IDAlumno: number;
+  Nombre: string;
+  Genero: string;
+  Grupo: string;
+}
+
 const Admin: React.FC = () => {
+  const [alumnos, setAlumnos] = useState<Alumno[]>([]);
+
+  const fetchAlumnos = async () => {
+    try {
+      const response = await fetch('http://0.0.0.0:8000/info_alumno');
+      const data = await response.json();
+      setAlumnos(data);
+    } catch (err) {
+      console.error('Error al obtener la información de los alumnos:', err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAlumnos();
+  }, []);
+
   return (
-    <h1>HOla fortnite como estas fortnite</h1>
-  )
+    <div className='bg-white font-serif min-h-screen flex flex-col'>
+      <div className="row-span-full flex justify-center items-center bg-azulInstitucional p-6 text-5xl text-white">
+        Admin
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 p-4 bg-gray-100">
+        {alumnos.map((alumno, index) => (
+          <Student
+            key={alumno.IDAlumno}
+            listNum={index + 1}
+            group={alumno.Grupo}
+            gender={alumno.Genero}
+            onDeleteButton={() => console.log(`Eliminar alumno ${alumno.IDAlumno}`)}
+            onEditButton={() => console.log(`Editar alumno ${alumno.IDAlumno}`)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Admin
+export default Admin;
--- FrontEnd/src/pages/Dashboard.tsx
+++ FrontEnd/src/pages/Dashboard.tsx
@@ -1,6 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+  const handleAdminClick = () => {
+      navigate('/admin'); 
+  };
+
   return (
-    <h1 className="bg-amber-300 text-blue-300 text-2xl font-semibold text-center">Dashboard</h1>
+    <>
+      <h1 className="bg-amber-300 text-blue-300 text-2xl font-semibold text-center">Dashboard</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleAdminClick} id="buttonAdmin">Admin</button>
+    </>
+
   )
 };
 
